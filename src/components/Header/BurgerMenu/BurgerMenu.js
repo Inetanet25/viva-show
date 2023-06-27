272206cf11a33ce6b6a36bdcf2e7fccd2c31aceb
Maksim Morozov <maksim-morozov-03@mail.ru>
2023-06-28 02:23:28 +0300

    Добавил обработку клика вне выезжающего меню для его закрытия

--- src/components/Header/BurgerMenu/BurgerMenu.js
+++ src/components/Header/BurgerMenu/BurgerMenu.js
@@ -1,19 +1,45 @@
 import './BurgerMenu.css';
 import {NavLink} from "react-router-dom";
+import {useRef, useEffect, useState} from "react";
+
 const BurgerMenu = () => {
 
+const navbarRef = useRef(null);
+const [isMenuOpen, setIsMenuOpen] = useState('close');
+const menuClass = isMenuOpen;
+function changeOfInput() {
 
-    return (<div className='burger-menu'>
-        <input type="checkbox"/>
+    setIsMenuOpen(prev => {
+        if (prev === 'open') return ('close')
+        else return ('open');
+    });
+}
+
+const checkBurger = document.getElementById('burger');
+useEffect(() => {
+    const ClickOutside = (e) => {
+        if ( navbarRef.current && navbarRef.current.contains(e.target) === false && isMenuOpen == 'open' && checkBurger.checked == true) {
+            setIsMenuOpen( 'close');
+            checkBurger.checked = false;
+        }
+    };
+    document.addEventListener('click', ClickOutside, true);
+    return() => {
+        document.removeEventListener('click', ClickOutside, true)
+    };
+}, [isMenuOpen])
+
+
+    return (<div className='burger-menu'  onClick={changeOfInput}>
+        <input id='burger' type="checkbox"/>
         <div className='hamburger-lines'>
             <span className='line line1'></span>
             <span className='line line2'></span>
             <span className='line line3'></span>
         </div>
 
-        <div className='navbar'>
-            <NavLink to='/' className={(e) => e.isActive ? 'active' : 'disable'}>Главная
-                страница</NavLink>
+        <div className={menuClass}  ref={navbarRef}>
+            <NavLink to='/' className={(e) => e.isActive ? 'active' : 'disable'}>Главная страница</NavLink>
             <NavLink to='/contacts' className={(e) => e.isActive ? 'active' : 'disable'}>Контакты</NavLink>
 
             <NavLink to='/videos' className={(e) => e.isActive ? 'active' : 'disable'}>Видео</NavLink>
